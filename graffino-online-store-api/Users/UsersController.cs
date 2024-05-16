fbcec6a5561ecd7128eb2a253e5799032b879a58
flcristian <qflorescucristian@gmail.com>
2024-05-16 15:41:13 +0300

    Created migrations and integrated tables into AppDbContext

--- graffino-online-store-api/Users/UsersController.cs
+++ graffino-online-store-api/Users/UsersController.cs
@@ -1,5 +1,6 @@
 using graffino_online_store_api.System.Constants;
 using graffino_online_store_api.Users.Controllers;
+using graffino_online_store_api.Users.DTOs;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,5 +20,26 @@ public class UsersController(
 
         IEnumerable<string> roles = await userManager.GetRolesAsync(user);
         return Ok(roles);
+    }
+
+    public override async Task<ActionResult<GetUserDetailsResponse>> GetUserDetails()
+    {
+        var user = await userManager.GetUserAsync(User);
+        if (user == null)
+        {
+            return NotFound(ExceptionMessages.USER_DOES_NOT_EXIST);
+        }
+
+        IEnumerable<string> roles = await userManager.GetRolesAsync(user);
+        GetUserDetailsResponse response = new GetUserDetailsResponse
+        {
+            Id = user.Id,
+            Name = user.UserName!,
+            Email = user.Email!,
+            EmailConfirmed = user.EmailConfirmed,
+            PhoneNumber = user.PhoneNumber!,
+            Roles = roles
+        };
+        return response;
     }
 }
