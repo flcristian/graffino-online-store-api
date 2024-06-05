289df24bf8532d2e5a8ebeae0c8ae88bccbfc21d
flcristian <qflorescucristian@gmail.com>
2024-06-05 21:47:38 +0300

    Changed build-publish

--- ci/build-publish.sh
+++ ci/build-publish.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
-
-# Ensuring that mandatory environment variables are set
+# Ensure mandatory environment variables are set
 : "${USERNAME:?USERNAME not set or empty}"
 : "${REPO:?REPO not set or empty}"
 : "${TAG:?TAG not set or empty}"
 
-# Create a new builder instance and use it
-docker buildx create --use
+# Source service_version_number.sh
+source ./service_version_number.sh
 
-# Build and push the Docker image
-docker buildx build \
-    --platform=linux/amd64,linux/arm64 \
-    -t "${USERNAME}/${REPO}:${TAG}" \
-    -t "${USERNAME}/${REPO}:latest" \
-    "${@:2}" \
-    --push \
-    "$1"
-    # Ensure this points to the directory with Dockerfile
+# Check if SERVICE_VERSION is defined
+if [ -z "$SERVICE_VERSION" ]; then
+    echo "Error: SERVICE_VERSION is not defined in service_version_number.sh."
+    exit 1
+fi
+
+echo "Building Docker image..."
+docker build -f Dockerfile -t ${REPO}:${SERVICE_VERSION} .
+
+echo "Getting the new image ID..."
+IMAGE_ID=$(docker images -q ${REPO}:${SERVICE_VERSION})
+echo "New image ID: $IMAGE_ID"
+
+echo "Tagging the image..."
+docker tag ${REPO}:${SERVICE_VERSION} ${USERNAME}/${REPO}:${SERVICE_VERSION}
+docker tag ${REPO}:${SERVICE_VERSION} ${USERNAME}/${REPO}:${TAG}
+
+echo "Pushing the tagged image..."
+docker push ${USERNAME}/${REPO}:${SERVICE_VERSION}
+docker push ${USERNAME}/${REPO}:${TAG}
+
+echo "Script completed."
