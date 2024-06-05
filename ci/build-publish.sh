48872f48808c0316c18aea63d9f14bd8307bef13
flcristian <qflorescucristian@gmail.com>
2024-06-05 21:57:39 +0300

    Fixed build-publish

--- ci/build-publish.sh
+++ ci/build-publish.sh
@@ -5,28 +5,18 @@
 : "${REPO:?REPO not set or empty}"
 : "${TAG:?TAG not set or empty}"
 
-# Source service_version_number.sh
-source ../service_version_number.sh
-
-# Check if SERVICE_VERSION is defined
-if [ -z "$SERVICE_VERSION" ]; then
-    echo "Error: SERVICE_VERSION is not defined in service_version_number.sh."
-    exit 1
-fi
 
 echo "Building Docker image..."
-docker build -f Dockerfile -t ${REPO}:${SERVICE_VERSION} .
+docker build -f Dockerfile -t ${REPO}:${TAG} .
 
 echo "Getting the new image ID..."
-IMAGE_ID=$(docker images -q ${REPO}:${SERVICE_VERSION})
+IMAGE_ID=$(docker images -q ${REPO}:${TAG})
 echo "New image ID: $IMAGE_ID"
 
 echo "Tagging the image..."
-docker tag ${REPO}:${SERVICE_VERSION} ${USERNAME}/${REPO}:${SERVICE_VERSION}
-docker tag ${REPO}:${SERVICE_VERSION} ${USERNAME}/${REPO}:${TAG}
+docker tag ${REPO}:${TAG} ${USERNAME}/${REPO}:${TAG}
 
 echo "Pushing the tagged image..."
-docker push ${USERNAME}/${REPO}:${SERVICE_VERSION}
 docker push ${USERNAME}/${REPO}:${TAG}
 
 echo "Script completed."
